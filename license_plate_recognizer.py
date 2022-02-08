e8d3a2fac2cd8af5a3bf8430f94c5bb17b380bb8
MZayed47 <zayedmashrukh@gmail.com>
2022-02-08 18:25:53 +0600

    Bangla license init

--- license_plate_recognizer.py
+++ license_plate_recognizer.py
@@ -9,57 +9,79 @@ import numpy as np
 # Example tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract'
 
 # point to license plate image (works well with custom crop function)
-gray = cv2.imread("./detections/crop/car3/license_plate_.png", 0)
+gray = cv2.imread("./detections/crop/test77/license_plate_1.png", 0)
 gray = cv2.resize( gray, None, fx = 3, fy = 3, interpolation = cv2.INTER_CUBIC)
 blur = cv2.GaussianBlur(gray, (5,5), 0)
 gray = cv2.medianBlur(gray, 3)
 # perform otsu thresh (using binary inverse since opencv contours work better with white text)
 ret, thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
-cv2.imshow("Otsu", thresh)
+# xx = cv2.resize(thresh, (800,400))
+cv2.imwrite("detections/cleared.jpg", thresh)
+cv2.imshow("Licese Plate", thresh)
 cv2.waitKey(0)
-rect_kern = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
 
-# apply dilation 
-dilation = cv2.dilate(thresh, rect_kern, iterations = 1)
-#cv2.imshow("dilation", dilation)
-#cv2.waitKey(0)
-# find contours
-try:
-    contours, hierarchy = cv2.findContours(dilation, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-except:
-    ret_img, contours, hierarchy = cv2.findContours(dilation, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-sorted_contours = sorted(contours, key=lambda ctr: cv2.boundingRect(ctr)[0])
-
-# create copy of image
-im2 = gray.copy()
-
-plate_num = ""
-# loop through contours and find letters in license plate
-for cnt in sorted_contours:
-    x,y,w,h = cv2.boundingRect(cnt)
-    height, width = im2.shape
-    
-    # if height of box is not a quarter of total height then skip
-    if height / float(h) > 6: continue
-    ratio = h / float(w)
-    # if height to width ratio is less than 1.5 skip
-    if ratio < 1.5: continue
-    area = h * w
-    # if width is not more than 25 pixels skip
-    if width / float(w) > 15: continue
-    # if area is less than 100 pixels skip
-    if area < 100: continue
-    # draw the rectangle
-    rect = cv2.rectangle(im2, (x,y), (x+w, y+h), (0,255,0),2)
-    roi = thresh[y-5:y+h+5, x-5:x+w+5]
-    roi = cv2.bitwise_not(roi)
-    roi = cv2.medianBlur(roi, 5)
-    #cv2.imshow("ROI", roi)
-    #cv2.waitKey(0)
-    text = pytesseract.image_to_string(roi, config='-c tessedit_char_whitelist=0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ --psm 8 --oem 3')
-    #print(text)
-    plate_num += text
-print(plate_num)
-cv2.imshow("Character's Segmented", im2)
-cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
+# # apply dilation
+# rect_kern = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
+# dilation = cv2.dilate(thresh, rect_kern, iterations = 1)
+# #cv2.imshow("dilation", dilation)
+# #cv2.waitKey(0)
+# # find contours
+# try:
+#     contours, hierarchy = cv2.findContours(dilation, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+# except:
+#     ret_img, contours, hierarchy = cv2.findContours(dilation, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+# sorted_contours = sorted(contours, key=lambda ctr: cv2.boundingRect(ctr)[0])
+
+# # create copy of image
+# im2 = gray.copy()
+
+# plate_num = ""
+# # loop through contours and find letters in license plate
+# for cnt in sorted_contours:
+#     x,y,w,h = cv2.boundingRect(cnt)
+#     height, width = im2.shape
+    
+#     # if height of box is not a quarter of total height then skip
+#     if height / float(h) > 6: continue
+#     ratio = h / float(w)
+#     # if height to width ratio is less than 1.5 skip
+#     if ratio < 1.5: continue
+#     area = h * w
+#     # if width is not more than 25 pixels skip
+#     if width / float(w) > 15: continue
+#     # if area is less than 100 pixels skip
+#     if area < 100: continue
+#     # draw the rectangle
+#     rect = cv2.rectangle(im2, (x,y), (x+w, y+h), (0,255,0),2)
+#     roi = thresh[y-5:y+h+5, x-5:x+w+5]
+#     roi = cv2.bitwise_not(roi)
+#     roi = cv2.medianBlur(roi, 5)
+#     #cv2.imshow("ROI", roi)
+#     #cv2.waitKey(0)
+#     text = pytesseract.image_to_string(roi, config='-c tessedit_char_whitelist=0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ --psm 8 --oem 3')
+#     # text = pytesseract.image_to_string(roi, config='-c -l ben --psm 6 --oem 3')
+#     #print(text)
+#     plate_num += text
+
+# print(plate_num)
+# cv2.imshow("Character's Segmented", im2)
+# cv2.waitKey(0)
+
+
+
+image = cv2.imread('detections/cleared.jpg')
+# gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# thresh = 255 - cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+# # Blur and perform text extraction(you can use raw image)
+# thresh = cv2.GaussianBlur(thresh, (3,3), 0)
+data = pytesseract.image_to_string(image, lang='ben', config='--psm 6 --oem 3')
+print(data)
+
+text_file = open("bangla_license.txt", "w", encoding='utf-8')
+n = text_file.write(data)
+text_file.close()
+
