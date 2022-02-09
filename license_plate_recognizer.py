714b4c72d9938153bca23676ed5619ee84765242
MZayed47 <zayedmashrukh@gmail.com>
2022-02-09 18:48:05 +0600

    bangla license 2 ways

--- license_plate_recognizer.py
+++ license_plate_recognizer.py
@@ -8,8 +8,10 @@ import numpy as np
 # pytesseract.pytesseract.tesseract_cmd = r'<full_path_to_your_tesseract_executable>'
 # Example tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract'
 
+plate = "./detections/crop/51/license_plate_1.png"
+
 # point to license plate image (works well with custom crop function)
-gray = cv2.imread("./detections/crop/test77/license_plate_1.png", 0)
+gray = cv2.imread(plate, 0)
 gray = cv2.resize( gray, None, fx = 3, fy = 3, interpolation = cv2.INTER_CUBIC)
 blur = cv2.GaussianBlur(gray, (5,5), 0)
 gray = cv2.medianBlur(gray, 3)
@@ -17,10 +19,8 @@ gray = cv2.medianBlur(gray, 3)
 ret, thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
 # xx = cv2.resize(thresh, (800,400))
 cv2.imwrite("detections/cleared.jpg", thresh)
-cv2.imshow("Licese Plate", thresh)
+cv2.imshow("Licese Plate 1", thresh)
 cv2.waitKey(0)
-
-cv2.destroyAllWindows()
 
 
 # # apply dilation
@@ -71,7 +71,6 @@ cv2.destroyAllWindows()
 # cv2.waitKey(0)
 
 
-
 image = cv2.imread('detections/cleared.jpg')
 # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # thresh = 255 - cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
@@ -79,9 +78,47 @@ image = cv2.imread('detections/cleared.jpg')
 # # Blur and perform text extraction(you can use raw image)
 # thresh = cv2.GaussianBlur(thresh, (3,3), 0)
 data = pytesseract.image_to_string(image, lang='ben', config='--psm 6 --oem 3')
+print("\nCleared:\n")
 print(data)
 
 text_file = open("bangla_license.txt", "w", encoding='utf-8')
-n = text_file.write(data)
+n = text_file.write(data+'\n')
+# text_file.close()
+
+
+
+###########################################################################################
+###########################################################################################
+
+in_image = cv2.imread(plate)
+s_file6 = 'temp/verifying.jpg'
+
+gray = cv2.cvtColor(in_image, cv2.COLOR_BGR2GRAY)
+gray = cv2.resize(gray, None, fx = 3, fy = 3, interpolation = cv2.INTER_CUBIC)
+blur = cv2.GaussianBlur(gray, (0,0), sigmaX=33, sigmaY=33)
+divide = cv2.divide(gray, blur, scale=255)
+thresh = cv2.threshold(divide, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
+morph = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+# in_image = cv2.resize(morph, (300, 300))
+cv2.imwrite(s_file6, morph)
+cv2.imshow("Licese Plate 2", morph)
+cv2.waitKey(0)
+
+
+image = cv2.imread('temp/verifying.jpg')
+# gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# thresh = 255 - cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+# # Blur and perform text extraction(you can use raw image)
+# thresh = cv2.GaussianBlur(thresh, (3,3), 0)
+data = pytesseract.image_to_string(image, lang='ben', config='--psm 6 --oem 3')
+print("\nVerified:\n")
+print(data)
+
+# text_file = open("bangla_license2.txt", "w", encoding='utf-8')
+n = text_file.write('\n' + data)
+
 text_file.close()
 
+cv2.destroyAllWindows()
